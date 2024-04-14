64539b9f1b4a1bab7183f69813ded3f6e2994734
ETEMESI <samweletemesi2@Gmail.com>
2024-04-15 01:21:08 +0300

    Done.

--- server/app.py
+++ server/app.py
@@ -1,6 +1,293 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-from flask import request, session
+# from flask import request, session
+# from flask_restful import Resource
+# from sqlalchemy.exc import IntegrityError
+
+# from config import app, db, api
+# from models import User, Recipe
+
+# class Signup(Resource):
+#     pass
+
+# class CheckSession(Resource):
+#     pass
+
+# class Login(Resource):
+#     pass
+
+# class Logout(Resource):
+#     pass
+
+# class RecipeIndex(Resource):
+#     pass
+
+# api.add_resource(Signup, '/signup', endpoint='signup')
+# api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+# api.add_resource(Login, '/login', endpoint='login')
+# api.add_resource(Logout, '/logout', endpoint='logout')
+# api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+# app.py
+
+# from flask import request, session
+# from flask_restful import Resource
+# from sqlalchemy.exc import IntegrityError
+
+# from config import app, db, api
+# from models import User, Recipe
+
+# class Signup(Resource):
+#     pass
+
+# class CheckSession(Resource):
+#     pass
+
+# class Login(Resource):
+#     pass
+
+# class Logout(Resource):
+#     pass
+
+# class RecipeIndex(Resource):
+#     pass
+
+# api.add_resource(Signup, '/signup', endpoint='signup')
+# api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+# api.add_resource(Login, '/login', endpoint='login')
+# api.add_resource(Logout, '/logout', endpoint='logout')
+# api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+
+# app.py
+
+# from flask import request, session, jsonify
+# from flask_restful import Resource
+# from sqlalchemy.exc import IntegrityError
+
+# from config import app, db, api
+# from models import User, Recipe
+
+# class Signup(Resource):
+#     def post(self):
+#         data = request.get_json()
+#         username = data.get('username')
+#         password = data.get('password')
+#         image_url = data.get('image_url')
+#         bio = data.get('bio')
+
+#         if not (username and password):
+#             return {'error': 'Username and password are required.'}, 422
+
+#         user = User.query.filter_by(username=username).first()
+#         if user:
+#             return {'error': 'Username already exists.'}, 422
+
+#         new_user = User(username=username, image_url=image_url, bio=bio)
+#         new_user.password_hash = password
+
+#         try:
+#             db.session.add(new_user)
+#             db.session.commit()
+#             session['user_id'] = new_user.id
+#             return jsonify(new_user.serialize()), 201
+#         except IntegrityError:
+#             db.session.rollback()
+#             return {'error': 'Error saving user to the database.'}, 422
+
+# class CheckSession(Resource):
+#     def get(self):
+#         user_id = session.get('user_id')
+#         if user_id:
+#             user = User.query.get(user_id)
+#             return jsonify(user.serialize()), 200
+#         else:
+#             return {'error': 'User not logged in.'}, 401
+
+# class Login(Resource):
+#     def post(self):
+#         data = request.get_json()
+#         username = data.get('username')
+#         password = data.get('password')
+
+#         user = User.query.filter_by(username=username).first()
+
+#         if user and user.check_password(password):
+#             session['user_id'] = user.id
+#             return jsonify(user.serialize()), 200
+#         else:
+#             return {'error': 'Invalid username or password.'}, 401
+
+# class Logout(Resource):
+#     def delete(self):
+#         if 'user_id' in session:
+#             session.pop('user_id')
+#             return '', 204
+#         else:
+#             return {'error': 'User not logged in.'}, 401
+
+# class RecipeIndex(Resource):
+#     def get(self):
+#         user_id = session.get('user_id')
+#         if user_id:
+#             recipes = Recipe.query.all()
+#             return jsonify([recipe.serialize() for recipe in recipes]), 200
+#         else:
+#             return {'error': 'User not logged in.'}, 401
+
+#     def post(self):
+#         user_id = session.get('user_id')
+#         if not user_id:
+#             return {'error': 'User not logged in.'}, 401
+
+#         data = request.get_json()
+#         title = data.get('title')
+#         instructions = data.get('instructions')
+#         minutes_to_complete = data.get('minutes_to_complete')
+
+#         if not (title and instructions and minutes_to_complete):
+#             return {'error': 'Title, instructions, and minutes_to_complete are required.'}, 422
+
+#         new_recipe = Recipe(title=title, instructions=instructions, minutes_to_complete=minutes_to_complete, user_id=user_id)
+
+#         try:
+#             db.session.add(new_recipe)
+#             db.session.commit()
+#             return jsonify(new_recipe.serialize()), 201
+#         except IntegrityError:
+#             db.session.rollback()
+#             return {'error': 'Error saving recipe to the database.'}, 422
+
+# api.add_resource(Signup, '/signup', endpoint='signup')
+# api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+# api.add_resource(Login, '/login', endpoint='login')
+# api.add_resource(Logout, '/logout', endpoint='logout')
+# api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+
+# app.py
+
+# from flask import request, session, jsonify
+# from flask_restful import Resource
+# from sqlalchemy.exc import IntegrityError
+
+# from config import app, db, api
+# from models import User, Recipe
+
+# class Signup(Resource):
+#     def post(self):
+#         data = request.get_json()
+#         username = data.get('username')
+#         password = data.get('password')
+#         image_url = data.get('image_url')
+#         bio = data.get('bio')
+
+#         if not (username and password):
+#             return {'error': 'Username and password are required.'}, 422
+
+#         user = User.query.filter_by(username=username).first()
+#         if user:
+#             return {'error': 'Username already exists.'}, 422
+
+#         new_user = User(username=username, image_url=image_url, bio=bio)
+#         new_user.password_hash = password
+
+#         try:
+#             db.session.add(new_user)
+#             db.session.commit()
+#             session['user_id'] = new_user.id
+#             return jsonify(new_user.serialize()), 201
+#         except IntegrityError as e:
+#             db.session.rollback()
+#             return {'error': 'Error saving user to the database.', 'details': str(e)}, 422
+
+# class CheckSession(Resource):
+#     def get(self):
+#         user_id = session.get('user_id')
+#         if user_id:
+#             user = User.query.get(user_id)
+#             return jsonify(user.serialize()), 200
+#         else:
+#             return {'error': 'User not logged in.'}, 401
+
+# class Login(Resource):
+#     def post(self):
+#         data = request.get_json()
+#         username = data.get('username')
+#         password = data.get('password')
+
+#         user = User.query.filter_by(username=username).first()
+
+#         if user and user.check_password(password):
+#             session['user_id'] = user.id
+#             return jsonify(user.serialize()), 200
+#         else:
+#             return {'error': 'Invalid username or password.'}, 401
+
+# class Logout(Resource):
+#     def delete(self):
+#         if 'user_id' in session:
+#             session.pop('user_id')
+#             return '', 204
+#         else:
+#             return {'error': 'User not logged in.'}, 401
+
+# class RecipeIndex(Resource):
+#     def get(self):
+#         user_id = session.get('user_id')
+#         if user_id:
+#             recipes = Recipe.query.filter_by(user_id=user_id).all()
+#             return jsonify([recipe.serialize() for recipe in recipes]), 200
+#         else:
+#             return {'error': 'User not logged in.'}, 401
+
+#     def post(self):
+#         user_id = session.get('user_id')
+#         if not user_id:
+#             return {'error': 'User not logged in.'}, 401
+
+#         data = request.get_json()
+#         title = data.get('title')
+#         instructions = data.get('instructions')
+#         minutes_to_complete = data.get('minutes_to_complete')
+
+#         if not (title and instructions and minutes_to_complete):
+#             return {'error': 'Title, instructions, and minutes_to_complete are required.'}, 422
+
+#         new_recipe = Recipe(title=title, instructions=instructions, minutes_to_complete=minutes_to_complete, user_id=user_id)
+
+#         try:
+#             db.session.add(new_recipe)
+#             db.session.commit()
+#             return jsonify(new_recipe.serialize()), 201
+#         except IntegrityError as e:
+#             db.session.rollback()
+#             return {'error': 'Error saving recipe to the database.', 'details': str(e)}, 422
+
+# api.add_resource(Signup, '/signup', endpoint='signup')
+# api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+# api.add_resource(Login, '/login', endpoint='login')
+# api.add_resource(Logout, '/logout', endpoint='logout')
+# api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+
+# app.py
+
+from flask import request, session, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,26 +295,101 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        image_url = data.get('image_url')
+        bio = data.get('bio')
+
+        if not (username and password):
+            return {'error': 'Username and password are required.'}, 422
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return {'error': 'Username already exists.'}, 422
+
+        new_user = User(username=username, image_url=image_url, bio=bio)
+        new_user.set_password(password)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            session['user_id'] = new_user.id
+            return jsonify(new_user.serialize()), 201
+        except IntegrityError as e:
+            db.session.rollback()
+            return {'error': 'Error saving user to the database.', 'details': str(e)}, 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            return jsonify(user.serialize()), 200
+        else:
+            return {'error': 'User not logged in.'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            return jsonify(user.serialize()), 200
+        else:
+            return {'error': 'Invalid username or password.'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if 'user_id' in session:
+            session.pop('user_id')
+            return '', 204
+        else:
+            return {'error': 'User not logged in.'}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            recipes = Recipe.query.filter_by(user_id=user_id).all()
+            return jsonify([recipe.serialize() for recipe in recipes]), 200
+        else:
+            return {'error': 'User not logged in.'}, 401
+
+    def post(self):
+        user_id = session.get('user_id')
+        if not user_id:
+            return {'error': 'User not logged in.'}, 401
+
+        data = request.get_json()
+        title = data.get('title')
+        instructions = data.get('instructions')
+        minutes_to_complete = data.get('minutes_to_complete')
+
+        if not (title and instructions and minutes_to_complete):
+            return {'error': 'Title, instructions, and minutes_to_complete are required.'}, 422
+
+        new_recipe = Recipe(title=title, instructions=instructions, minutes_to_complete=minutes_to_complete, user_id=user_id)
+
+        try:
+            db.session.add(new_recipe)
+            db.session.commit()
+            return jsonify(new_recipe.serialize()), 201
+        except IntegrityError as e:
+            db.session.rollback()
+            return {'error': 'Error saving recipe to the database.', 'details': str(e)}, 422
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,3 +1,20 @@
+# from sqlalchemy.ext.hybrid import hybrid_property
+# from sqlalchemy_serializer import SerializerMixin
+
+# from config import db, bcrypt
+
+# class User(db.Model, SerializerMixin):
+#     __tablename__ = 'users'
+
+#     pass
+
+# class Recipe(db.Model, SerializerMixin):
+#     __tablename__ = 'recipes'
+    
+#     pass
+
+# models.py
+
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 
@@ -5,10 +22,33 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    _password_hash = db.Column(db.String(128), nullable=False)
+    image_url = db.Column(db.String(200))
+    bio = db.Column(db.String(500))
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
 
-    pass
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('password_hash is not readable')
+
+    @password_hash.setter
+    def password_hash(self, plaintext_password):
+        self._password_hash = bcrypt.generate_password_hash(plaintext_password).decode('utf-8')
+
+    def check_password(self, plaintext_password):
+        return bcrypt.check_password_hash(self._password_hash, plaintext_password)
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    instructions = db.Column(db.String(1000), nullable=False)
+    minutes_to_complete = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # Constraints
+    __table_args__ = (
+        db.CheckConstraint('CHAR_LENGTH(instructions) >= 50', name='instructions_length'),
+    )
